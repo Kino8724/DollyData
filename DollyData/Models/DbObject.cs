f1838323a81c28b76285e19e31fe6a4a8e0be9fb
Wyatt Ptak <ptakwyatt@gmail.com>
2024-05-09 13:20:47 -0700

    Started adding models

---
+++ DollyData/Models/DbObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DollyData.Models
+{
+    public class DbObject
+    {
+        /// <summary>
+        /// Gets or sets the database id.
+        /// </summary>
+        public Guid Id { get; set; } = Guid.NewGuid();
+    }
+}
---
+++ DollyData/Models/Doll.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DollyData.Models
+{
+    public class Doll : DbObject
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public string company { get; set; }
+        public string lineName { get; set; }
+        public int amount { get; set; }
+        public bool isFavorite {  get; set; }
+
+
+        public Doll()
+        {
+            
+        }
+    }
+}
